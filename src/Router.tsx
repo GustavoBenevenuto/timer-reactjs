801692af3879e9715410951781d29b14636ac24e
Gustavo Benevenuto <gustavo.benesilva@hotmail.com>
2024-09-21 09:20:16 -0300

    Configurando Layout Padrão

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,13 +2,16 @@ import { Route, Routes } from "react-router-dom";
 import { Home } from "./Pages/Home";
 import { Historico } from "./Pages/Historico";
 import { Erro } from "./Pages/Erro";
+import { DefaultLayout } from "./layouts/DefaultLayout";
 
 export function Router() {
     return (
         <Routes>
-            <Route path="" Component={Home}/>
-            <Route path="/historico" Component={Historico}/>
-            <Route path="*" Component={Erro}/>
-        </Routes>    
+            <Route path="/" element={<DefaultLayout/>}>
+                <Route path="" Component={Home} />
+                <Route path="/historico" Component={Historico} />
+                <Route path="*" Component={Erro} />
+            </Route>
+        </Routes>
     )
 }
