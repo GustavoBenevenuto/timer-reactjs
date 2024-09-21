6b59555cc32bd2fc901bfa6e1f2acc745fc76990
Gustavo Benevenuto <gustavo.benesilva@hotmail.com>
2024-09-21 08:44:31 -0300

    Inclusão de fontes e refatoração

--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -1,5 +1,20 @@
 export const defaultTheme = {
-    primeiro: 'purple',
-    segundo: 'orange',
     branco_01: '#FFF',
+    cinza_100: '#E1E1E6',
+    cinza_300: '#C4C4CC',
+    cinza_400: '#8D8D99',
+    cinza_500: '#7C7C8A',
+    cinza_600: '#323238',
+    cinza_700: '#29292E',
+    cinza_800: '#202024',
+    cinza_900: '#121214',
+
+    verde_300: '#00B37E',
+    verde_500: '#00875F',
+    verde_700: '#015F43',
+
+    vermelho_500: '#AB222E',
+    vermelho_700: '#7A1921',
+
+    amarelo_500: '#FBA94C',
 }
