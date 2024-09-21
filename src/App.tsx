31b885b56c616583b11a038e0e3eaef789bfc9ce
Gustavo Benevenuto <gustavo.benesilva@hotmail.com>
2024-09-21 08:18:41 -0300

    Adicionando styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import { Button } from "./components/Button"
 
 function App() {
 
     return (
         <>
-
+            <h1>Hello World</h1>
+            <>
+                <Button variant="primary" />
+                <Button variant="secondary" />
+                <Button variant="success" />
+                <Button variant="danger" />
+                <Button />
+            </>
         </>
     )
 }
