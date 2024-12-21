51278b953de7943f02de8a4fdc23bd69ebabd60a
Gustavo Benevenuto <gustavo.benesilva@hotmail.com>
2024-12-21 20:13:11 -0300

    Concluindo tela de histórico

--- src/Pages/Historico/index.tsx
+++ src/Pages/Historico/index.tsx
@@ -1,5 +1,12 @@
+import { useContext } from 'react'
 import { HistoryContainer, HistoryList, Status } from './styles'
+import { CicloContext } from '../../context/CicloContext'
+import { formatDistanceToNow } from 'date-fns'
+import { ptBR } from 'date-fns/locale/pt-BR'
+
 export function Historico() {
+    const { ciclos } = useContext(CicloContext)
+
     return (
         <HistoryContainer>
             <h1>Meu histórico</h1>
@@ -9,67 +16,38 @@ export function Historico() {
                         <tr>
                             <th>Tarefa</th>
                             <th>Duração</th>
-                            <th>Duração</th>
+                            <th>Inicio</th>
                             <th>Status</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>Tarefa</td>
-                            <td>20 minutos</td>
-                            <td>Há 2 meses</td>
-                            <td>
-                                <Status statusColor="amarelo">Em andamento</Status>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Tarefa</td>
-                            <td>20 minutos</td>
-                            <td>Há 2 meses</td>
-                            <td>
-                                <Status statusColor="vermelho">Interrompido</Status>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Tarefa</td>
-                            <td>20 minutos</td>
-                            <td>Há 2 meses</td>
-                            <td>
-                                <Status statusColor="verde">Concluído</Status>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Tarefa</td>
-                            <td>20 minutos</td>
-                            <td>Há 2 meses</td>
-                            <td>
-                                <Status statusColor="verde">Concluído</Status>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Tarefa</td>
-                            <td>20 minutos</td>
-                            <td>Há 2 meses</td>
-                            <td>
-                                <Status statusColor="verde">Concluído</Status>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Tarefa</td>
-                            <td>20 minutos</td>
-                            <td>Há 2 meses</td>
-                            <td>
-                                <Status statusColor="verde">Concluído</Status>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Tarefa</td>
-                            <td>20 minutos</td>
-                            <td>Há 2 meses</td>
-                            <td>
-                                <Status statusColor="verde">Concluído</Status>
-                            </td>
-                        </tr>
+                        {ciclos.map(ciclo => {
+                            return (
+                                <tr key={ciclo.id}>
+                                    <td>{ciclo.task}</td>
+                                    <td>{ciclo.minutes} minutos</td>
+                                    <td>
+                                        {formatDistanceToNow(ciclo.dataInicial, {
+                                            addSuffix: true,
+                                            locale: ptBR,
+                                        })}
+                                    </td>
+                                    <td>
+                                        {ciclo.dataConcluida && (
+                                            <Status statusColor="verde">Concluído</Status>
+                                        )}
+
+                                        {ciclo.dataInterrompida && (
+                                            <Status statusColor="vermelho">Interrompido</Status>
+                                        )}
+
+                                        {!ciclo.dataConcluida && !ciclo.dataInterrompida && (
+                                            <Status statusColor="amarelo">Em andamento</Status>
+                                        )}
+                                    </td>
+                                </tr>
+                            )
+                        })}
                     </tbody>
                 </table>
             </HistoryList>
