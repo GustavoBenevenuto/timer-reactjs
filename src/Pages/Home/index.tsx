7a12f4b3e14dc30f09e965998131574f0e5c7034
Gustavo Benevenuto <gustavo.benesilva@hotmail.com>
2024-12-21 10:42:10 -0300

    Aprimorando inputs

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -10,12 +10,22 @@ export function Home() {
                     <TaskInput
                         id="task"
                         placeholder="Dê um nome para o seu projeto"
+                        list="sugestao-task"
                     />
+                    <datalist id="sugestao-task">
+                        <option value="Projeto 1"/>
+                        <option value="Projeto 2"/>
+                        <option value="Projeto 3"/>
+                        <option value="Corrida"/>
+                    </datalist>
                     <label htmlFor="minutesAmount">durante</label>
                     <MinutesInput
                         type="number"
                         id="minutesAmount"
                         placeholder="00"
+                        step="5"
+                        min="5"
+                        max="60"
                     />
                     <span>minutos.</span>
                 </FormContainer>
