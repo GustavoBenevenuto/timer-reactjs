963d4a6ad77a157438e7aeca07de4b958827422e
Gustavo Benevenuto <gustavo.benesilva@hotmail.com>
2024-12-21 15:19:09 -0300

    Adicionando react-hook-form e zod

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,22 +1,42 @@
 import { Play } from "phosphor-react";
 import { CountdownContainer, FormContainer, HomeContainer, MinutesInput, Separator, StartButton, TaskInput } from "./styles";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as zod from "zod";
+
+const validacaoSchema = zod.object({
+    task: zod.string().min(1, 'Informe a tarefa'),
+    minutes: zod
+        .number()
+        .min(5, 'O ciclo precisa ser de no mínimo 5 minutos.')
+        .max(60, 'O ciclo precisa ser de no máximo 60 minutos.'),
+})
 
 export function Home() {
+    const { register, handleSubmit, watch, formState } = useForm({
+        resolver: zodResolver(validacaoSchema),
+    })
+
+    function salvar(form: any) {
+        console.log({ form })
+    }
+
     return (
         <HomeContainer>
-            <form>
+            <form onSubmit={handleSubmit(salvar)}>
                 <FormContainer>
                     <label htmlFor="task">Vou trabalhar em</label>
                     <TaskInput
                         id="task"
                         placeholder="Dê um nome para o seu projeto"
                         list="sugestao-task"
+                        {...register('task')}
                     />
                     <datalist id="sugestao-task">
-                        <option value="Projeto 1"/>
-                        <option value="Projeto 2"/>
-                        <option value="Projeto 3"/>
-                        <option value="Corrida"/>
+                        <option value="Projeto 1" />
+                        <option value="Projeto 2" />
+                        <option value="Projeto 3" />
+                        <option value="Corrida" />
                     </datalist>
                     <label htmlFor="minutesAmount">durante</label>
                     <MinutesInput
@@ -26,6 +46,7 @@ export function Home() {
                         step="5"
                         min="5"
                         max="60"
+                        {...register('minutes', { valueAsNumber: true })}
                     />
                     <span>minutos.</span>
                 </FormContainer>
