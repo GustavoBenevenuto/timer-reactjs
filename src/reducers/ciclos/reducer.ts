d193480a33b0341a6625af29ff3cde0109abd919
Gustavo Benevenuto <gustavo.benesilva@hotmail.com>
2024-12-22 11:42:54 -0300

    Utilizando immer

--- src/reducers/ciclos/reducer.ts
+++ src/reducers/ciclos/reducer.ts
@@ -1,3 +1,4 @@
+import { produce } from "immer";
 import { ActionTypesEnum } from "./actions";
 
 export interface ICiclo {
@@ -17,37 +18,57 @@ interface ICiclosState {
 export function ciclosReducer(state: ICiclosState, action: any) {
     switch (action.type) {
         case ActionTypesEnum.ADD_NOVO_CICLO: {
-            return {
-                ...state,
-                idCicloAtivo: action.payload.novoCiclo.id,
-                ciclos: [...state.ciclos, action.payload.novoCiclo]
-            }
+            // return {
+            //     ...state,
+            //     idCicloAtivo: action.payload.novoCiclo.id,
+            //     ciclos: [...state.ciclos, action.payload.novoCiclo]
+            // }
+            return produce(state, draft => {
+                draft.ciclos.push(action.payload.novoCiclo)
+                draft.idCicloAtivo = action.payload.novoCiclo.id
+            })
         }
         case ActionTypesEnum.PARAR_CICLO: {
-            return {
-                ...state,
-                idCicloAtivo: null,
-                ciclos: state.ciclos.map((ciclo) => {
-                    if (ciclo.id === state.idCicloAtivo) {
-                        return { ...ciclo, dataInterrompida: new Date() }
-                    } else {
-                        return ciclo
-                    }
-                }),
-            }
+            // return {
+            //     ...state,
+            //     idCicloAtivo: null,
+            //     ciclos: state.ciclos.map((ciclo) => {
+            //         if (ciclo.id === state.idCicloAtivo) {
+            //             return { ...ciclo, dataInterrompida: new Date() }
+            //         } else {
+            //             return ciclo
+            //         }
+            //     }),
+            // }
+            return produce(state, draft => {
+                draft.idCicloAtivo = null
+                const indexCiclo = draft.ciclos.findIndex(cicloItem => cicloItem.id == state.idCicloAtivo)
+                if(indexCiclo < 0) {
+                    return state
+                }
+                draft.ciclos[indexCiclo].dataInterrompida = new Date()
+            })
         }
         case ActionTypesEnum.FINALIZA_CICLO_ATUAL: {
-            return {
-                ...state,
-                idCicloAtivo: null,
-                ciclos: state.ciclos.map((ciclo) => {
-                    if (ciclo.id === state.idCicloAtivo) {
-                        return { ...ciclo, dataConcluida: new Date() }
-                    } else {
-                        return ciclo
-                    }
-                }),
-            }
+            // return {
+            //     ...state,
+            //     idCicloAtivo: null,
+            //     ciclos: state.ciclos.map((ciclo) => {
+            //         if (ciclo.id === state.idCicloAtivo) {
+            //             return { ...ciclo, dataConcluida: new Date() }
+            //         } else {
+            //             return ciclo
+            //         }
+            //     }),
+            // }
+            return produce(state, draft => {
+                draft.idCicloAtivo = null
+                const indexCiclo = draft.ciclos.findIndex(cicloItem => cicloItem.id == state.idCicloAtivo)
+                if(indexCiclo < 0) {
+                    return state
+                }
+                draft.ciclos[indexCiclo].dataConcluida = new Date()
+            })
         }
         default: {
             return state
